2957bb49fe91c207f615bc8e53033f5a2b696d87
hoangvietanhh <haongvietanh1797@gmail.com>
2021-11-30 23:45:26 +0700

    navbar doing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Router, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar } from './components';
-
+import './App.css';
 
 export const App = () => {
     return (
         <div className="app">
             <div className="navbar">
-
+                <Navbar />
             </div>
             <div className="main">
 
